d1e67d46cff8bac9c72f69bbf459241031d5e3c1
Maria Zubareva <maria.zubareva@insolar.io>
2020-03-16 16:18:23 +0300

    PENV-3: insconfig

--- insolard/launchnet.sh
+++ insolard/launchnet.sh
@@ -3,6 +3,7 @@ set -em
 # requires: lsof, awk, sed, grep, pgrep, docker
 
 export GO111MODULE=on
+export GOFLAGS="-mod=vendor"
 
 # Changeable environment variables (parameters)
 INSOLAR_ARTIFACTS_DIR=${INSOLAR_ARTIFACTS_DIR:-".artifacts"}/
@@ -189,7 +190,7 @@ generate_insolard_configs()
 {
     echo "generate configs"
     set -x
-    go run -mod=vendor insolar-scripts/generate_insolar_configs.go -p ${INSGORUND_PORT_FILE}
+    go run insolar-scripts/generate_insolar_configs.go -p ${INSGORUND_PORT_FILE}
     { set +x; } 2>/dev/null
 }
 
@@ -411,6 +412,7 @@ if [[ "$LOGROTATOR_ENABLE" == "1" ]]; then
   build_logger
 fi
 
+HEAVY_DB="badger"
 if [[ "$POSTGRES_ENABLE" == "1" ]]; then
   # Terminate running PostgreSQL container if there is one
   docker stop insolar-postgresql || true
@@ -429,6 +431,7 @@ if [[ "$POSTGRES_ENABLE" == "1" ]]; then
     echo "PostgreSQL is not up yet, retrying..."
     sleep 1
   done
+  HEAVY_DB="postgres"
 fi
 
 handle_sigchld()
@@ -440,16 +443,18 @@ handle_sigchld()
 trap 'handle_sigchld' SIGCHLD
 
 echo "Running genesis before actually starting any nodes (consensus may fail if genesis takes long)"
-$INSOLARD \
+$INSOLARD heavy \
     --config ${DISCOVERY_NODES_DATA}1/insolard.yaml \
     --heavy-genesis ${HEAVY_GENESIS_CONFIG_FILE} \
+    --database=$HEAVY_DB \
     --genesis-only
 
 echo "start heavy node"
 set -x
-$INSOLARD \
+$INSOLARD heavy \
     --config ${DISCOVERY_NODES_DATA}1/insolard.yaml \
     --heavy-genesis ${HEAVY_GENESIS_CONFIG_FILE} \
+    --database=$HEAVY_DB \
     2>&1 | ${LOGROTATOR} ${DISCOVERY_NODE_LOGS}1/output.log > /dev/null &
 { set +x; } 2>/dev/null
 echo "heavy node started in background"
@@ -458,8 +463,17 @@ echo "log: ${DISCOVERY_NODE_LOGS}1/output.log"
 echo "start discovery nodes ..."
 for i in `seq 2 $NUM_DISCOVERY_NODES`
 do
+    ROLE=""
+    # even - lme, odd - vm
+    if [ $((i%2)) -eq 0 ]
+    then
+      ROLE="virtual"
+    else
+      ROLE="light"
+    fi
+
     set -x
-    $INSOLARD \
+    $INSOLARD $ROLE \
         --config ${DISCOVERY_NODES_DATA}${i}/insolard.yaml \
         2>&1 | ${LOGROTATOR} ${DISCOVERY_NODE_LOGS}${i}/output.log > /dev/null &
     { set +x; } 2>/dev/null
